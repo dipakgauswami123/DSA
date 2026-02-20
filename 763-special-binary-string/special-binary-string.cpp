c0cddd4da65c3b40d013559952ad46ceff243110
dipak gauswami <gauswamidipak8@gmail.com>
2026-02-20 20:50:11 +0530

    Time: 2 ms (34.67%) | Memory: 9.4 MB (34.67%) - LeetSync

---
+++ 763-special-binary-string/special-binary-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string solve(string s){
+        vector<string> spacials;
+        int start = 0;
+        int sum = 0;
+        for (int i = 0; i< s.length(); i++){
+            sum += s[i] == '1' ? 1 : -1;
+            if(sum == 0){
+                string inner = s.substr(start+1, i-start-1);
+                spacials.push_back("1"+solve(inner) +"0");
+                start = i+1;
+            }
+        }
+        sort(begin(spacials),end(spacials), greater<string>());
+        string result;
+        for(string &str : spacials){
+            result += str;
+        }
+        return result;
+    }
+    string makeLargestSpecial(string s) {
+        return solve(s);
+    }
+};
